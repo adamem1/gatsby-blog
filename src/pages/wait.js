a87b0270bcaf46ae94238b514426ed7004bf31e4
Adam Martinetti <adam.em@gmail.com>
2021-03-19 13:00:58 -0700

    Add basic waiting page

---
+++ src/pages/wait.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Home() {
+  return (
+    <main>
+      <h1>We've passed the waiting room.</h1>
+      <p>
+        We've made it to the page after the waiting room.
+      </p>
+    </main>
+  )
+}
